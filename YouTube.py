4373c5b75f625c7709d8789c5988d610dd90bf8e
Yathartsalil <kelkaryatharth1@gmail.com>
2025-12-02 16:17:17 +0400

    Add files via upload

---
+++ YouTube.py
@@ -0,0 +1,92 @@
+name = "Yatharth Kelkar"
+
+print(name[0])
+print(name[14])
+
+print()
+
+# string[start : stop : step]
+
+text = "MasterClass"
+# Positive Indexing
+print(text[0:6])
+print(text[6:])
+print(text[6:11])
+
+print()
+
+# Negative Indexing
+print(text[-5:])
+print(text[:-5])
+
+print()
+
+print(text[-5:-11])
+print(text[-11:-5])
+
+print()
+
+# Changing Case
+YouTube = "Yatharth"
+print(YouTube.upper())
+print(YouTube.lower())
+print(YouTube.title())
+
+print()
+
+# Removing unwanted spaces 
+msg = " Hi How Are You? "
+print(msg)
+print(msg.strip())
+print(msg.lstrip())
+print(msg.rstrip())
+print(msg.replace("H", "J"))
+
+print()
+
+# Finding Strings and Substrings
+text = "A paragraph is a distinct section of writing that typically consists of a group of related sentences focused on a single idea or topic. It serves to organize thoughts and provide clarity in writing, often marked by indentation at the beginning. While there is no strict rule on the number of sentences in a paragraph, the emphasis should be on the unity and coherence of ideas rather than length. In academic and formal writing, paragraphs help structure essays, research papers, and other documents, making them easier to read and understand"
+print(text.find("single"))
+
+print()
+
+# Replacing Text
+text1 = "I love python"
+print(text1.replace("python","Java"))
+
+print()
+
+name1 = "09090909"
+print(name1.isalpha())   # Alpha is for string
+
+number = "yyy"
+print(number.isdigit()) # Digit is for numbers
+
+mix = "abc123"
+print(mix.isalnum())   # Alnum is for both string and digits 
+
+print()
+
+#F - strings
+name2 = "Yatharth Kelkar"
+age = 16
+message = "Hi You are {} and you are {} years old."
+print(message.format(name2, age))
+
+print()
+
+pi = 3.14159
+print(f"{pi:.2f}")
+
+print()
+
+print(f"{name2:>10}")
+print(f"{name2:<10}")
+
+print()
+
+#Practice 1
+name = input("Enter your name: ")
+hobby = input("Enter your hobby: ")
+Country = input("Enter your country: ")
+print(f"hello {name},your favourite hobby is {hobby} and you live in the counrty of {Country}")
